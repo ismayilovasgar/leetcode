861635c0dd1645030aec785e17c86d237e9b1499
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-05-14 23:15:13 +0400

    finish_task

--- easy/1431/1431.py
+++ easy/1431/1431.py
@@ -1,3 +1,13 @@
 # 1431. Kids With the Greatest Number of Candies
+class Solution:
+    def kidsWithCandies(self, candies: list[int], extraCandies: int):
+        max_candies = max(candies)
+        return [i + extraCandies >= max_candies for i in candies]
 
 
+candies = [2, 3, 5, 1, 3]
+extraCandies = 3
+solution = Solution()
+result = solution.kidsWithCandies(candies, extraCandies)
+print(result)
+# [True, True, True, False, True]
