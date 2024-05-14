d293ac89505a809afdbbd1a3c30ae419d869b582
ismayilovasgar <ismayilovv.asgarr.21@hotmail.com>
2024-05-14 23:15:13 +0400

    finish_task

---
+++ easy/605/605.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: list[int], n: int) -> bool:
+        for i, flower in enumerate(flowerbed):
+            if flower == 0 and (i == 0 or flowerbed[i - 1] == 0) and (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0):
+                flowerbed[i] = 1
+                n -= 1
+            if n <= 0:
+                return True
+
+        return False
+
+
+solution = Solution()
+result = solution.canPlaceFlowers([1, 1, 0, 0, 0, 1, 1], 1)
+print(result)
+# Example 1:
+# Input: flowerbed = [1,0,0,0,1], n = 1
+# Output: true
+#
+# Example 2:
+# Input: flowerbed = [1,0,0,0,1], n = 2
+# Output: false
